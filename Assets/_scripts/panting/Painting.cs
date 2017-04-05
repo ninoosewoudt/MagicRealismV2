c81612d0664bf88fa5e8d97f128c509341bf5ccb
brianboersen <19965@ma-web.nl>
2017-04-05 11:32:03 +0200

    als je een kleur en een object mee geeft word hij gekleurd

---
+++ Assets/_scripts/panting/Painting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    /*    SteamVR_TrackedController[] TrackedControllers;
+
+        void Start()
+        {
+            TrackedControllers = FindObjectsOfType<SteamVR_TrackedController>();
+            foreach(var c in TrackedControllers)
+            {
+                Debug.Log(c.transform.name);
+            }
+        }
+        */
+
+    public void changeColor(GameObject gameobj,Color color)
+    {
+        Renderer rend = gameobj.GetComponent<Renderer>();
+        rend.material.color = color;
+    }
+
+    public void changeMaterial(GameObject gameobj, Material mat)
+    {
+        Renderer rend = gameobj.GetComponent<Renderer>();
+        rend.material = mat;
+    }
+}
---
+++ Assets/_scripts/panting/TestPainting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPainting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject testobj;
+    private Painting painting;
+	
+	void Start ()
+    {
+        painting = GetComponent<Painting>();
+        painting.changeColor(testobj,new Color(1,0.2f,0,0));
+	}
+	
+	void Update () {
+		
+	}
+}
