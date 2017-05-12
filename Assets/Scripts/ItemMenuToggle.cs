9c7414a2c64acb114861b823c5a9d1ba13f23eb6
nino.osewoudt@gmail.com <nino.osewoudt@gmail.com>
2017-05-12 09:29:17 +0100

    .

--- Assets/Scripts/ItemMenuToggle.cs
+++ Assets/Scripts/ItemMenuToggle.cs
@@ -8,15 +8,35 @@ public class ItemMenuToggle : MonoBehaviour {
 	private Player player;
 	[SerializeField]
 	private GameObject itemMenuPrefab; 
+	[SerializeField]
+	private GameObject itemMenuItemPrefab;
 	private GameObject itemMenu;
 	private bool menuEnabled; 
 
 	private bool init;
 
+	public List<GameObject> Objects;
+
 	void Start () {
+		//Objects = new List<GameObject> ();
 
 		itemMenu = Instantiate (itemMenuPrefab);
 
+		for (int y = 0; y < 1; y++) {
+			for (int x = 0; x < 2; x++) {
+				var ypos = y * 10;
+				var xpos = x * 10;
+				var item = Instantiate (itemMenuItemPrefab);
+				item.transform.SetParent (itemMenu.transform,false);	
+				item.transform.position = new Vector3(xpos,ypos);
+				item.GetComponent<ItemMenuItem>().Item = Objects[y+x];
+			}
+		}
+
+		Objects.ForEach ((go) => {
+			
+
+		});
 
 		Debug.Log (FindObjectsOfType<ItemMenuToggle> ().Length);
 
@@ -33,9 +53,9 @@ public class ItemMenuToggle : MonoBehaviour {
 			return;
 		if (player.leftHand.controller.GetPressUp (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
 			if (init) {
-				itemMenu.transform.parent = player.leftHand.transform;
-				itemMenu.transform.position = player.leftHand.transform.position + new Vector3(0,+0.4f,+0.4f);
-				itemMenu.transform.rotation = player.leftHand.transform.rotation;
+				itemMenu.transform.SetParent (player.leftHand.transform,false);
+				itemMenu.transform.position += new Vector3(0,+0.4f,+0.4f);
+				//itemMenu.transform.rotation = player.leftHand.transform.rotation;
 
 				init = false;
 			}
