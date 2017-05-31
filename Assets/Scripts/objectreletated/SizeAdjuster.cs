68a40a2d2702298d9722fd228e8dcec97d698d42
brianboersen <19965@ma-web.nl>
2017-05-31 16:37:55 +0200

    prullen bak verkleind en verwijderd

---
+++ Assets/Scripts/objectreletated/SizeAdjuster.cs
@@ -0,0 +1,100 @@
+﻿//Brian Boersen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeAdjuster : MonoBehaviour
+{
+    private enum sizeState
+    {
+        idle = 0,
+        shrinking = 1,
+        growing
+    }
+
+    [SerializeField]
+    private float changeTime = 2f;
+    private float changeStep,currentstep;
+
+    private IEnumerator coroutine;
+
+    private Vector3 mySize;
+
+    private sizeState state;
+
+    private void Start()
+    {
+        state = sizeState.idle;
+        mySize = gameObject.transform.localScale;
+        changeStep = 1f / (changeTime / 0.033f);
+    }
+
+    public void changeSize(Vector3 newSize)
+    {
+        newSize = RealObjectSize.fitInToSize(gameObject,newSize);
+        StartCoroutine(toNewSize(newSize));
+    }
+
+    public void oldsize()
+    {
+        StartCoroutine(toOldSize(mySize));
+    }
+
+    private IEnumerator toNewSize(Vector3 newSize)
+    {
+        while (true)
+        {
+            print(state);
+
+            if (state == sizeState.growing)
+            {
+                yield return new WaitForSeconds(0.033f);
+            }
+            else if (state == sizeState.idle)
+            {
+                state = sizeState.shrinking;
+                currentstep = 0;
+            }
+
+            currentstep += changeStep;
+
+            gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale, newSize, currentstep);
+
+            if (newSize == gameObject.transform.localScale)
+            {
+                state = sizeState.idle;
+                StopCoroutine(toNewSize(newSize));
+            }
+
+            yield return new WaitForSeconds(0.033f);
+        }        
+    }
+
+    private IEnumerator toOldSize(Vector3 newSize)
+    {
+        while (true)
+        {
+            if (state == sizeState.shrinking)
+            {
+                yield return new WaitForSeconds(0.033f);
+            }
+            else if (state == sizeState.idle)
+            {
+                state = sizeState.growing;
+                currentstep = 0;
+            }
+
+            currentstep += changeStep;
+
+            gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale, newSize, currentstep);
+
+            if (newSize == gameObject.transform.localScale)
+            {
+                state = sizeState.idle;
+                StopCoroutine(toOldSize(newSize));
+            }
+
+            yield return new WaitForSeconds(0.033f);
+        }       
+    }
+}
--- Assets/Scripts/trashcan/ShrinkOnTouch.cs
+++ Assets/Scripts/trashcan/ShrinkOnTouch.cs
@@ -6,52 +6,22 @@ using UnityEngine;
 public class ShrinkOnTouch : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 shrinkSize = new Vector3(.4f, .4f, .4f);
-
-    private Dictionary<GameObject, Vector3> originalSize = new Dictionary<GameObject, Vector3>();
-
-    private List<GameObject> gameobjects = new List<GameObject>();
+    private Vector3 newSize = new Vector3(0.5f,0.5f,0.5f);
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Paintable"))
+        if (other.CompareTag("Paintable") && other.gameObject.GetComponent<SizeAdjuster>() != null)
         {
-            print("shrink");
-
-            originalSize.Add(other.gameObject, other.gameObject.transform.localScale);
-
-            gameobjects.Add(other.gameObject);
-
-            //other.gameObject.transform.localScale = Vector3.Lerp(other.gameObject.transform.localScale, RealObjectSize.fitInToSize(other.gameObject, shrinkSize), 3);
+            other.gameObject.GetComponent<SizeAdjuster>().changeSize(newSize);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (gameobjects.Contains(other.gameObject))
+        if (other.CompareTag("Paintable") && other.gameObject.GetComponent<SizeAdjuster>() != null)
         {
-            gameobjects.Remove(other.gameObject);
-
-            other.gameObject.transform.localScale = originalSize[other.gameObject];
-
-            originalSize.Remove(other.gameObject);
+            //other.gameObject.GetComponent<SizeAdjuster>().oldsize();
         }
     }
 
-    private void Update()
-    {
-        for(int i = 0; i < gameobjects.Count; i++)
-        {
-            GameObject currentObject = gameobjects[i];
-
-            if(currentObject == null)
-            {
-                gameobjects.Remove(currentObject);
-                return;
-            }
-
-            currentObject.transform.localScale = Vector3.Lerp(currentObject.transform.localScale, RealObjectSize.fitInToSize(currentObject, shrinkSize), 0.6f);
-        }
-       
-    }
 }
