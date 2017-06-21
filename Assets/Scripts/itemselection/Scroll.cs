5c59fa7c3986b875e5245cb0eb3ec2414e847114
brianboersen <19965@ma-web.nl>
2017-06-21 10:38:41 +0200

    bezig met scrollen voor tube

---
+++ Assets/Scripts/itemselection/Scroll.cs
@@ -0,0 +1,60 @@
+﻿//Brian Boersen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Scroll : MonoBehaviour
+{
+    private Player player;
+    private Hand gripedHand;
+
+    private TubeMenu tubeMenu;
+
+    private bool gripped = false;
+
+
+	void Start ()
+    {
+        player = Player.instance;
+        tubeMenu = GetComponent<TubeMenu>();
+	}
+	
+	void FixedUpdate ()
+    {
+        checkGriped();
+
+        if (gripped)
+        {
+            scrollWithController();
+        }
+	}
+
+    private void checkGriped()
+    {
+        if(player.leftHand != null)
+        if (player.leftHand.controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            gripedHand = player.leftHand;
+            gripped = true;
+            return;
+        }
+
+        if (player.rightHand != null)
+        if (player.rightHand.controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            gripedHand = player.rightHand;
+            gripped = true;
+            return;
+        }
+
+        gripped = false;
+    }
+
+    private void scrollWithController()
+    {
+
+    }
+
+
+}
