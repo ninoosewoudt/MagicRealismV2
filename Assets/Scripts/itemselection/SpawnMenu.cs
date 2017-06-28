4734e2ff7e73a1c8d4610fa150c9d292a79e5bed
brianboersen <19965@ma-web.nl>
2017-06-28 13:22:02 +0200

    bezig met buis en prullenbak spawnen

--- Assets/Scripts/itemselection/SpawnMenu.cs
+++ Assets/Scripts/itemselection/SpawnMenu.cs
@@ -11,14 +11,18 @@ public class SpawnMenu : MonoBehaviour
     private LineRenderer lineRend;
 
     [SerializeField]
-    private GameObject prullenbak,buis;
+    private GameObject trashcan,tube,floor;
+
+    [SerializeField]
+    private float tubeSpawnHeight = 1;
 
     [SerializeField]
     private float lineWith = 0.04f;
 
     [SerializeField]
     private Color goodColor,badColor;
-    private Color currentColor;
+
+    private bool canSpawn = false;
 
     ///voor test
     [SerializeField]
@@ -31,14 +35,11 @@ public class SpawnMenu : MonoBehaviour
     void Awake()
     {
         player = Player.instance;
-        currentColor = goodColor;
 
         if(GetComponent<LineRenderer>() == null)
         lineConvig();
 
         lineRend = GetComponent<LineRenderer>();
-
-        setLineColor();
     }
 	
 	void Update ()
@@ -50,10 +51,17 @@ public class SpawnMenu : MonoBehaviour
 
 
         // als menu knop word ingedrukt
-        if (player.leftHand != null && player.leftHand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        if (player.leftHand == null)
         {
-            lineRend.SetPosition(0, player.leftHand.transform.position);
-            lineRend.SetPosition(1, player.leftHand.transform.forward * 10);
+
+        }
+        else if(player.leftHand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            drawLine();
+        }
+        else if (canSpawn && player.leftHand.controller.GetPressUp(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            spawnObjects(checkFloorHit());
         }
 	}
 
@@ -64,8 +72,38 @@ public class SpawnMenu : MonoBehaviour
         lineRend.startWidth = lineWith;
     }
 
-    private void setLineColor()
+    private void spawnObjects(Vector3 pos)
     {
-        lineRend.material.color = currentColor;
+        Vector3 rot = player.leftHand.transform.eulerAngles;
+        rot = new Vector3(rot.x, rot.y + 180, rot.z);
+
+        Instantiate(trashcan, new Vector3(pos.x -0.2f, 0, pos.z), Quaternion.Euler(rot));
+
+        Instantiate(trashcan, new Vector3(pos.x + 0.2f, tubeSpawnHeight, pos.z), Quaternion.Euler(rot));
     }
+
+    private void drawLine(Color color)
+    {
+        lineRend.material.color = color;
+        lineRend.SetPosition(0, player.leftHand.transform.position);
+        lineRend.SetPosition(1, player.leftHand.transform.forward * 10);
+    }
+
+    private Vector3 checkFloorHit()
+    {
+        RaycastHit hit;
+        /////////////////////////////////////////////////////of Vector3.forward
+        if (Physics.Raycast(player.leftHand.transform.position, -Vector3.up, out hit))
+        {
+            canSpawn = true;
+            drawLine(goodColor);
+            return hit.point;
+        }
+
+        canSpawn = false;
+        drawLine(badColor);
+        return Vector3.zero;
+    }
+
+    
 }
