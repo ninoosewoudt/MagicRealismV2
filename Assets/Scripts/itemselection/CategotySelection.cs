822d7c2be70b24bcf7067e4ed22b467abdcf012d
brianboersen <19965@ma-web.nl>
2017-07-05 16:35:43 +0200

    spawn tube gefixed

---
+++ Assets/Scripts/itemselection/CategotySelection.cs
@@ -0,0 +1,19 @@
+﻿//Brian Boersen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategotySelection : MonoBehaviour
+{
+    private float lastRotation;
+
+	void Awake ()
+    {
+        lastRotation = this.gameObject.transform.localRotation.x;
+	}
+
+	void FixedUpdate ()
+    {
+		
+	}
+}
